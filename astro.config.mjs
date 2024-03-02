566bedb1277560be3008e7761ef53045c7f54051
RafetTanriverdi <by.rafet.tanriverdi@gmail.com>
2024-03-02 20:37:48 +0300

    added new skills on the home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import tailwind from '@astrojs/tailwind'
+import compress from 'astro-compress'
+import icon from "astro-icon"
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  compressHTML: true,
+  integrations: [mdx(), icon(), tailwind({
+    applyBaseStyles: false,
+  }), compress()],
+})
